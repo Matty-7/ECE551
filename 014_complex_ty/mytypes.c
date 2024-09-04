c6666cf47cc7e81d8cc4e36bbad663a3dced7fa4
jh730 <jh730@duke.edu>
2024-09-04 18:11:37 -0400

    did 014

---
+++ 014_complex_ty/mytypes.c
@@ -0,0 +1,8 @@
+struct complex_num_tag {
+  double real_part;
+  double imaginary_part;
+};
+typedef struct complex_num_tag complex_num_t;
+
+enum animal_tag { CAT, Fish, SNAKE, DOG, HORSE };
+typedef struct animal_t animal_tag;
