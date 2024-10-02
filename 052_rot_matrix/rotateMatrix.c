69e7832b131d1f9650a7b8bba29a1aab7666a69a
jh730 <jh730@duke.edu>
2024-10-02 19:53:17 -0400

    did 052

--- 052_rot_matrix/rotateMatrix.c
+++ 052_rot_matrix/rotateMatrix.c
@@ -8,64 +8,58 @@
 
 int main(int argc, char ** argv) {
   char matrix[ROWS][COLS];
-  char pch[TOTAL_SIZE];
+  char char_buffer[TOTAL_SIZE];
   int c;
   int index = 0;
 
-  // 检查命令行参数数量
   if (argc != 2) {
     fprintf(stderr, "error\n");
     return EXIT_FAILURE;
   }
 
-  // 打开文件
-  FILE * f = fopen(argv[1], "r");
-  if (f == NULL) {
+  FILE * file = fopen(argv[1], "r");
+  if (file == NULL) {
     fprintf(stderr, "error\n");
     return EXIT_FAILURE;
   }
 
-  // 逐个字符读取文件并检查
-  while ((c = fgetc(f)) != EOF) {
+  while ((c = fgetc(file)) != EOF) {
     if (index >= TOTAL_SIZE) {
       fprintf(stderr, "error\n");
-      fclose(f);
+      fclose(file);
       return EXIT_FAILURE;
     }
     if (c >= 128) {
       fprintf(stderr, "error\n");
-      fclose(f);
+      fclose(file);
       return EXIT_FAILURE;
     }
     if (c == '\n' && index % (COLS + 1) != COLS) {
       fprintf(stderr, "error\n");
-      fclose(f);
+      fclose(file);
       return EXIT_FAILURE;
     }
     if (c != '\n' && index % (COLS + 1) == COLS) {
       fprintf(stderr, "error\n");
-      fclose(f);
+      fclose(file);
       return EXIT_FAILURE;
     }
-    pch[index] = c;
+    char_buffer[index] = c;
     index++;
   }
 
-  // 检查文件长度
   if (index != TOTAL_SIZE) {
     fprintf(stderr, "error\n");
-    fclose(f);
+    fclose(file);
     return EXIT_FAILURE;
   }
 
-  // 矩阵旋转
   for (int i = 0; i < ROWS; i++) {
     for (int j = 0; j < COLS; j++) {
-      matrix[j][ROWS - 1 - i] = pch[i * (COLS + 1) + j];
+      matrix[j][ROWS - 1 - i] = char_buffer[i * (COLS + 1) + j];
     }
   }
 
-  // 输出旋转后的矩阵
   for (int i = 0; i < ROWS; i++) {
     for (int j = 0; j < COLS; j++) {
       fprintf(stdout, "%c", matrix[i][j]);
@@ -73,8 +67,7 @@ int main(int argc, char ** argv) {
     fprintf(stdout, "\n");
   }
 
-  // 关闭文件
-  if (fclose(f) != 0) {
+  if (fclose(file) != 0) {
     fprintf(stderr, "error\n");
     return EXIT_FAILURE;
   }
