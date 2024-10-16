1561429b780f782c944cb1bf890c4ed7db0ae0b4
jh730 <jh730@duke.edu>
2024-10-16 00:37:16 -0400

    062

--- 060_outname/outname.c
+++ 060_outname/outname.c
@@ -5,17 +5,8 @@
 #include <string.h>
 
 char * computeOutputFileName(const char * inputName) {
-  //WRITE ME
-  size_t inputLength = strlen(inputName);
-
-  char * outputName = malloc((inputLength + strlen(".counts") + 1) * sizeof(*outputName));
-
-  if (outputName == NULL) {
-    perror("Unable to allocate memory for output file name");
-    return NULL;
-  }
-
-  strcpy(outputName, inputName);
-  strcat(outputName, ".counts");
-  return outputName;
+  size_t len = strlen(inputName) + 8;
+  char * outName = malloc(len);
+  snprintf(outName, len, "%s.counts", inputName);
+  return outName;
 }
