0eb4dc3162f4541a78132ed2cd981832c0857555
jh730 <jh730@duke.edu>
2024-10-17 11:02:12 -0400

    did 067

--- 067_vector_2d/vector.hpp
+++ 067_vector_2d/vector.hpp
@@ -1,3 +1,30 @@
 /* write your class interface in this file
    write function definitions only if they are very short
  */
+
+#ifndef VECTOR_HPP
+#define VECTOR_HPP
+
+#include <cmath>
+#include <cstdio>
+
+class Vector2D {
+ private:
+  double x;
+  double y;
+
+ public:
+  void initVector(double init_x, double init_y);
+
+  double getMagnitude() const;
+
+  Vector2D operator+(const Vector2D & rhs) const;
+
+  Vector2D & operator+=(const Vector2D & rhs);
+
+  double dot(const Vector2D & rhs) const;
+
+  void print() const;
+};
+
+#endif
