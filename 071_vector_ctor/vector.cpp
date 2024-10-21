b50f9b54f26a4bf03329ead461d9d36d1d56174f
jh730 <jh730@duke.edu>
2024-10-21 17:37:04 -0400

    did 071

--- 071_vector_ctor/vector.cpp
+++ 071_vector_ctor/vector.cpp
@@ -6,30 +6,22 @@
 /* write your class implementation in this file
  */
 
-void Vector2D::initVector(double init_x, double init_y) {
-  x = init_x;
-  y = init_y;
-}
-
 double Vector2D::getMagnitude() const {
   return std::sqrt(x * x + y * y);
 }
 
 Vector2D Vector2D::operator+(const Vector2D & rhs) const {
-  Vector2D result;
-  result.x = this->x + rhs.x;
-  result.y = this->y + rhs.y;
-  return result;
+  return Vector2D(x + rhs.x, y + rhs.y);
 }
 
 Vector2D & Vector2D::operator+=(const Vector2D & rhs) {
-  this->x += rhs.x;
-  this->y += rhs.y;
+  x += rhs.x;
+  y += rhs.y;
   return *this;
 }
 
 double Vector2D::dot(const Vector2D & rhs) const {
-  return this->x * rhs.x + this->y * rhs.y;
+  return x * rhs.x + y * rhs.y;
 }
 
 void Vector2D::print() const {
