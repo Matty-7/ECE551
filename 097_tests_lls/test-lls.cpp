94d9f26862689b8bc0643aad6a03ca4a8691640e
jh730 <jh730@duke.edu>
2024-11-11 17:15:45 -0500

    did 097

--- 097_tests_lls/test-lls.cpp
+++ 097_tests_lls/test-lls.cpp
@@ -7,142 +7,151 @@
 
 class Tester {
  public:
-  // testing for default constructor is done for you
-  void testDefCtor() {
-    IntList il;
-    assert(il.head == NULL);
-    assert(il.tail == NULL);
-    assert(il.getSize() == 0);
-  }
-  // example of another method you might want to write
-  void testAddFront() {
-    // WRITE ME
-    IntList il;
-    il.addFront(10);
-    assert(il.head != NULL);
-    assert(il.tail != NULL);
-    assert(il.head->data == 10);
-    assert(il.tail->data == 10);
-    assert(il.getSize() == 1);
+  void testDefCtor();
+  void testAddFront();
+  void testAddBack();
+  void testRemove();
+  void testOperatorAccess();
+  void testFind();
+  void testGetSize();
+};
 
-    il.addFront(20);
-    assert(il.head->data == 20);
-    assert(il.tail->data == 10);
-    assert(il.getSize() == 2);
+// testing for default constructor is done for you
+void Tester::testDefCtor() {
+  IntList il;
+  assert(il.head == NULL);
+  assert(il.tail == NULL);
+  assert(il.getSize() == 0);
+}
+// example of another method you might want to write
+void Tester::testAddFront() {
+  // WRITE ME
+  IntList il;
+  il.addFront(10);
+  assert(il.head != NULL);
+  assert(il.tail != NULL);
+  assert(il.head->data == 10);
+  assert(il.tail->data == 10);
+  assert(il.getSize() == 1);
 
-    il.addFront(30);
-    assert(il.head->data == 30);
-    assert(il.head->next->data == 20);
-    assert(il.tail->data == 10);
-    assert(il.getSize() == 3);
-  }
-  // many more tester methods
+  il.addFront(20);
+  assert(il.head->data == 20);
+  assert(il.tail->data == 10);
+  assert(il.getSize() == 2);
 
-  void testAddBack() {
-    IntList il;
-    il.addBack(10);
-    assert(il.head != NULL);
-    assert(il.tail != NULL);
-    assert(il.head->data == 10);
-    assert(il.tail->data == 10);
-    assert(il.getSize() == 1);
+  il.addFront(30);
+  assert(il.head->data == 30);
+  assert(il.head->next->data == 20);
+  assert(il.tail->data == 10);
+  assert(il.getSize() == 3);
+}
+// many more tester methods
 
-    il.addBack(20);
-    assert(il.head->data == 10);
-    assert(il.tail->data == 20);
-    assert(il.getSize() == 2);
+void Tester::testAddBack() {
+  IntList il;
+  il.addBack(10);
+  assert(il.head != NULL);
+  assert(il.tail != NULL);
+  assert(il.head->data == 10);
+  assert(il.tail->data == 10);
+  assert(il.getSize() == 1);
 
-    il.addBack(30);
-    assert(il.head->data == 10);
-    assert(il.head->next->data == 20);
-    assert(il.tail->data == 30);
-    assert(il.getSize() == 3);
-  }
+  il.addBack(20);
+  assert(il.head->data == 10);
+  assert(il.tail->data == 20);
+  assert(il.getSize() == 2);
 
-  void testRemove() {
-    IntList il;
-    il.addBack(10);
-    il.addBack(20);
-    il.addBack(30);
-    il.addBack(40);
+  il.addBack(30);
+  assert(il.head->data == 10);
+  assert(il.head->next->data == 20);
+  assert(il.tail->data == 30);
+  assert(il.getSize() == 3);
+}
 
-    assert(il.remove(20) == true);
-    assert(il.head->data == 10);
-    assert(il.head->next->data == 30);
-    assert(il.tail->data == 40);
-    assert(il.getSize() == 3);
+void Tester::testRemove() {
+  IntList il;
+  il.addBack(10);
+  il.addBack(20);
+  il.addBack(30);
+  il.addBack(40);
 
-    assert(il.remove(10) == true);
-    assert(il.head->data == 30);
-    assert(il.getSize() == 2);
+  assert(il.remove(20) == true);
+  assert(il.head->data == 10);
+  assert(il.head->next->data == 30);
+  assert(il.tail->data == 40);
+  assert(il.getSize() == 3);
 
-    assert(il.remove(40) == true);
-    assert(il.head->data == 30);
-    assert(il.tail->data == 30);
-    assert(il.getSize() == 1);
+  assert(il.remove(10) == true);
+  assert(il.head->data == 30);
+  assert(il.getSize() == 2);
 
-    assert(il.remove(30) == true);
-    assert(il.head == NULL);
-    assert(il.tail == NULL);
-    assert(il.getSize() == 0);
+  assert(il.remove(40) == true);
+  assert(il.head->data == 30);
+  assert(il.tail->data == 30);
+  assert(il.getSize() == 1);
 
-    assert(il.remove(50) == false);
-  }
+  assert(il.remove(30) == true);
+  assert(il.head == NULL);
+  assert(il.tail == NULL);
+  assert(il.getSize() == 0);
 
-  void testOperatorAccess() {
-    IntList il;
-    il.addBack(10);
-    il.addBack(20);
-    il.addBack(30);
+  assert(il.remove(50) == false);
+}
 
-    assert(il[0] == 10);
-    assert(il[1] == 20);
-    assert(il[2] == 30);
+void Tester::testOperatorAccess() {
+  IntList il;
+  il.addBack(10);
+  il.addBack(20);
+  il.addBack(30);
 
-    il[1] = 25;
-    assert(il[1] == 25);
-  }
+  assert(il[0] == 10);
+  assert(il[1] == 20);
+  assert(il[2] == 30);
 
-  void testFind() {
-    IntList il;
-    il.addBack(10);
-    il.addBack(20);
-    il.addBack(30);
+  il[1] = 25;
+  assert(il[1] == 25);
+}
 
-    assert(il.find(10) == 0);
-    assert(il.find(20) == 1);
-    assert(il.find(30) == 2);
-    assert(il.find(40) == -1);
-  }
+void Tester::testFind() {
+  IntList il;
+  il.addBack(10);
+  il.addBack(20);
+  il.addBack(30);
 
-  void testGetSize() {
-    IntList il;
-    assert(il.getSize() == 0);
+  assert(il.find(10) == 0);
+  assert(il.find(20) == 1);
+  assert(il.find(30) == 2);
+  assert(il.find(40) == -1);
+}
 
-    il.addFront(10);
-    assert(il.getSize() == 1);
+void Tester::testGetSize() {
+  IntList il;
+  assert(il.getSize() == 0);
 
-    il.addBack(20);
-    assert(il.getSize() == 2);
+  il.addFront(10);
+  assert(il.getSize() == 1);
 
-    il.remove(10);
-    assert(il.getSize() == 1);
+  il.addBack(20);
+  assert(il.getSize() == 2);
 
-    il.remove(20);
-    assert(il.getSize() == 0);
-  };
+  il.remove(10);
+  assert(il.getSize() == 1);
 
-  int main(void) {
-    Tester t;
-    t.testDefCtor();
-    t.testAddFront();
-    t.testAddBack();
-    t.testRemove();
-    t.testOperatorAccess();
-    t.testFind();
-    t.testGetSize();
-    std::cout << "All tests passed!!" << std::endl;
+  il.remove(20);
+  assert(il.getSize() == 0);
+};
 
-    // write calls to your other test methods here
-    return EXIT_SUCCESS;
-  }
+int main(void) {
+  Tester t;
+  t.testDefCtor();
+  t.testAddFront();
+  t.testAddBack();
+  t.testRemove();
+  t.testOperatorAccess();
+  t.testFind();
+  t.testGetSize();
+  std::cout << "All tests passed!!" << std::endl;
+
+  // write calls to your other test methods here
+  return EXIT_SUCCESS;
+}
