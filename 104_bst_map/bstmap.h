926aa7f358a5fabf8e76102a3f4c0868c38fd7bf
jh730 <jh730@duke.edu>
2024-11-15 14:09:10 -0500

    did 104

--- 104_bst_map/bstmap.h
+++ 104_bst_map/bstmap.h
@@ -1,125 +1,127 @@
-#ifndef __BSTMAP_H__
-#define __BSTMAP_H__
-
+#include <cstdio>
+#include <cstdlib>
+#include <exception>
 #include <iostream>
-#include <stdexcept>
+#include <utility>
 
 #include "map.h"
+using namespace std;
 
 template<typename K, typename V>
 class BstMap : public Map<K, V> {
- private:
-  struct Node {
-    K key;
-    V value;
+  class Node {
+   public:
+    pair<K, V> data;
     Node * left;
     Node * right;
-    Node(const K & key, const V & value) :
-        key(key), value(value), left(NULL), right(NULL) {}
+    Node() : data(0, 0), left(NULL), right(NULL) {}
+    Node(K k, V v) : data(k, v), left(NULL), right(NULL) {}
   };
-
   Node * root;
-
-  Node * add(Node * current, const K & key, const V & value) {
-    if (current == NULL) {
-      return new Node(key, value);
-    }
-    if (key < current->key) {
-      current->left = add(current->left, key, value);
-    }
-    else if (key > current->key) {
-      current->right = add(current->right, key, value);
-    }
-    else {
-      current->value = value;
-    }
-    return current;
-  }
-
-  const V & lookup(Node * current, const K & key) const throw(std::invalid_argument) {
-    if (current == NULL) {
-      throw std::invalid_argument("Key not found");
-    }
-    if (key == current->key) {
-      return current->value;
-    }
-    else if (key < current->key) {
-      return lookup(current->left, key);
-    }
-    else {
-      return lookup(current->right, key);
-    }
-  }
-
-  Node * remove(Node * current, const K & key) {
-    if (current == NULL) {
-      throw std::invalid_argument("Key not found");
-    }
-    if (key < current->key) {
-      current->left = remove(current->left, key);
-    }
-    else if (key > current->key) {
-      current->right = remove(current->right, key);
-    }
-    else {
-      if (current->left == NULL) {
-        Node * temp = current->right;
-        delete current;
-        return temp;
-      }
-      else if (current->right == NULL) {
-        Node * temp = current->left;
-        delete current;
-        return temp;
-      }
-      else {
-        Node * successor = findMin(current->right);
-        current->key = successor->key;
-        current->value = successor->value;
-        current->right = remove(current->right, successor->key);
-      }
-    }
-    return current;
-  }
-
-  Node * findMin(Node * current) {
-    while (current->left != NULL) {
-      current = current->left;
-    }
-    return current;
-  }
-
-  void destroy(Node * current) {
-    if (current != NULL) {
-      destroy(current->left);
-      destroy(current->right);
-      delete current;
-    }
-  }
 
  public:
   BstMap() : root(NULL) {}
-
-  virtual ~BstMap() { destroy(root); }
-
-  void add(const K & key, const V & value) { root = add(root, key, value); }
-
-  const V & lookup(const K & key) const throw(std::invalid_argument) {
+  BstMap(const BstMap & m) : root(NULL) { copyTree(&root, m.root); }
+  void copyTree(Node ** n, const Node * m) {
+    if (m != NULL) {
+      *n = new Node(m->data.first, m->data.second);
+      copyTree(&(*n)->left, m->left);
+      copyTree(&(*n)->right, m->right);
+    }
+  }
+  virtual void add(const K & key, const V & value) {
+    Node ** current = &root;
+    while (*current != NULL) {
+      if ((*current)->data.first == key) {
+        (*current)->data.second = value;
+        return;
+      }
+      else if ((*current)->data.first > key) {
+        current = &((*current)->left);
+      }
+      else
+        current = &((*current)->right);
+    }
+    *current = new Node(key, value);
+  }
+  virtual const V & lookup(const K & key) const throw(std::invalid_argument) {
     Node * current = root;
     while (current != NULL) {
-      if (key == current->key) {
-        return current->value;
+      if (current->data.first == key) {
+        return current->data.second;
       }
-      else if (key < current->key) {
+      else if (current->data.first > key) {
         current = current->left;
       }
       else {
         current = current->right;
       }
     }
-    throw std::invalid_argument("Key not found");
+    throw std::invalid_argument("key does not exist");
   }
-
-  void remove(const K & key) { root = remove(root, key); }
+  Node ** find(const K & key) {
+    Node ** current = &root;
+    while (*current != NULL) {
+      if ((*current)->data.first == key) {
+        return current;
+      }
+      else if ((*current)->data.first > key) {
+        current = &(*current)->left;
+      }
+      else {
+        current = &(*current)->right;
+      }
+    }
+    return NULL;
+  }
+  const BstMap & operator=(const BstMap & m) {
+    if (&m != this) {
+      BstMap temp = m;
+      Node * t = root;
+      root = temp.root;
+      temp.root = t;
+    }
+    return *this;
+  }
+  virtual void remove(const K & key) {
+    Node ** N = find(key);
+    Node * N2 = *N;
+    if (N2 == NULL) {
+      return;
+    }
+    if (N2->left == NULL) {
+      *N = N2->right;
+    }
+    else {
+      Node ** vic = &N2->left;
+      while ((*vic)->right != NULL) {
+        vic = &(*vic)->right;
+      }
+      *N = *vic;
+      *vic = (*vic)->left;
+      (*N)->left = N2->left;
+      (*N)->right = N2->right;
+    }
+    delete N2;
+  }
+  void freeBst(Node * r) {
+    if (r == NULL) {
+      return;
+    }
+    freeBst(r->left);
+    freeBst(r->right);
+    delete r;
+  }
+  Node * getRoot() { return root; }
+  void print(Node * r) {
+    if (r == NULL)
+      return;
+    print(r->left);
+    cout << r->data.first << " " << r->data.second << endl;
+    print(r->right);
+  }
+  virtual ~BstMap() {
+    freeBst(root);
+    root = NULL;
+  }
 };
-#endif
