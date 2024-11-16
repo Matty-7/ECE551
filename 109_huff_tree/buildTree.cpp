b260e0a381a729415d4dac56152c168224e21153
jh730 <jh730@duke.edu>
2024-11-16 12:51:21 -0500

    did 109

--- 109_huff_tree/buildTree.cpp
+++ 109_huff_tree/buildTree.cpp
@@ -1,5 +1,27 @@
+#include <queue>
+
 #include "node.h"
 
-Node *  buildTree(uint64_t * counts) {
+Node * buildTree(uint64_t * counts) {
   //WRITE ME!
-}  
+
+  priority_queue_t pq;
+
+  for (int i = 0; i < 256; i++) {
+    if (counts[i] > 0) {
+      pq.push(new Node(i, counts[i]));
+    }
+  }
+
+  while (pq.size() > 1) {
+    Node * left = pq.top();
+    pq.pop();
+    Node * right = pq.top();
+    pq.pop();
+
+    Node * parent = new Node(left, right);
+    pq.push(parent);
+  }
+
+  return pq.top();
+}
