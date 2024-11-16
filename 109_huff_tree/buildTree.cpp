9a51be50e1f4d46664181375fabbc76b2ae47ee1
jh730 <jh730@duke.edu>
2024-11-16 13:01:19 -0500

    did 109

--- 109_huff_tree/buildTree.cpp
+++ 109_huff_tree/buildTree.cpp
@@ -10,8 +10,6 @@ Node * buildTree(uint64_t * counts) {
 
   for (unsigned i = 0; i < 257; i++) {
     if (counts[i] != 0) {
-      std::cout << "Creating node for symbol: " << i << " with frequency: " << counts[i]
-                << std::endl;
       pq.push(new Node(i, counts[i]));
     }
   }
@@ -21,8 +19,6 @@ Node * buildTree(uint64_t * counts) {
     pq.pop();
     Node * n2 = pq.top();
     pq.pop();
-    std::cout << "Combining nodes with frequencies: " << n1->freq << " and " << n2->freq
-              << std::endl;
     Node * n3 = new Node(n1, n2);
     pq.push(n3);
   }
