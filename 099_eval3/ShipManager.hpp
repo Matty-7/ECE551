6b13dece91bd1c3c6f1426854c3d6cb4c97e9771
jh730 <jh730@duke.edu>
2024-11-17 13:32:21 -0500

    did step1

--- 099_eval3/ShipManager.hpp
+++ 099_eval3/ShipManager.hpp
@@ -1,24 +1,26 @@
 #ifndef SHIPMANAGER_HPP
 #define SHIPMANAGER_HPP
 
-#include <string>
+#include <stdint.h>
+
 #include <map>
+#include <string>
 
 class ShipManager {
-public:
-    bool loadShipsFromFile(const std::string& filename);
-    void printRouteCapacities() const;
+ public:
+  bool loadShipsFromFile(const std::string & filename);
+  void printRouteCapacities() const;
 
-private:
-    struct Ship {
-        std::string name;
-        std::string source;
-        std::string destination;
-        uint64_t capacity;
-    };
+ private:
+  struct Ship {
+    std::string name;
+    std::string source;
+    std::string destination;
+    uint64_t capacity;
+  };
 
-    bool parseShipLine(const std::string& line, Ship& ship);
-    std::map<std::string, uint64_t> routeCapacities;
+  bool parseShipLine(const std::string & line, Ship & ship);
+  std::map<std::string, uint64_t> routeCapacities;
 };
 
-#endif // SHIPMANAGER_HPP
+#endif  // SHIPMANAGER_HPP
