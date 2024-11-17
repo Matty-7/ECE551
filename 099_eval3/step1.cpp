6c0b3a39302daffcd82e2413d8ef0d83c78dc939
jh730 <jh730@duke.edu>
2024-11-17 13:35:24 -0500

    fix

--- 099_eval3/step1.cpp
+++ 099_eval3/step1.cpp
@@ -1,17 +1,19 @@
+#include <cstdlib>
 #include <iostream>
+
 #include "ShipManager.hpp"
 
-int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " <ships_file>" << std::endl;
-        return EXIT_FAILURE;
-    }
+int main(int argc, char * argv[]) {
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <ships_file>" << std::endl;
+    return EXIT_FAILURE;
+  }
 
-    ShipManager manager;
-    if (!manager.loadShipsFromFile(argv[1])) {
-        return EXIT_FAILURE;
-    }
+  ShipManager manager;
+  if (!manager.loadShipsFromFile(argv[1])) {
+    return EXIT_FAILURE;
+  }
 
-    manager.printRouteCapacities();
-    return EXIT_SUCCESS;
+  manager.printRouteCapacities();
+  return EXIT_SUCCESS;
 }
