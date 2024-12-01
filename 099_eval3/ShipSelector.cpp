1026fa4104016ee600185ca6f6713d9eca5e01e5
Matty-7 <jh730@duke.edu>
2024-12-01 13:37:39 -0500

    did step4

--- 099_eval3/ShipSelector.cpp
+++ 099_eval3/ShipSelector.cpp
@@ -1,42 +1,44 @@
 #include "ShipSelector.hpp"
 #include <limits>
-#include <iostream>
 
 ShipSelector::ShipSelector(AVLMultiMap<uint64_t, Ship*, std::less<uint64_t>, ShipNameCompare> & shipMap)
     : shipMap(shipMap) {
 }
 
 Ship * ShipSelector::findBestShip(const Cargo & cargo) {
-    std::vector<std::pair<std::pair<uint64_t, std::set<Ship*, ShipNameCompare> >, int> > nodes = shipMap.preOrderDump();
+    const std::vector<std::pair<std::pair<uint64_t, std::set<Ship*, ShipNameCompare> >, int> > & nodes = shipMap.preOrderDump();
     
     Ship* bestShip = NULL;
     uint64_t bestExtraCapacity = std::numeric_limits<uint64_t>::max();
     
-    
-    for (size_t i = 0; i < nodes.size(); ++i) {
-        uint64_t currentCapacity = nodes[i].first.first;
+    for (std::vector<std::pair<std::pair<uint64_t, std::set<Ship*, ShipNameCompare> >, int> >::const_iterator nodeIt = nodes.begin();
+         nodeIt != nodes.end();
+         ++nodeIt) {
+        
+        const uint64_t currentCapacity = nodeIt->first.first;
         if (currentCapacity < cargo.weight) {
-            continue;  
+            continue;
         }
         
-        uint64_t extraCapacity = currentCapacity - cargo.weight;
+        const uint64_t extraCapacity = currentCapacity - cargo.weight;
         if (extraCapacity >= bestExtraCapacity) {
-            continue;  
+            continue;
         }
         
-        const std::set<Ship*, ShipNameCompare> & ships = nodes[i].first.second;
-        for (std::set<Ship*, ShipNameCompare>::const_iterator shipIt = ships.begin(); 
-             shipIt != ships.end(); 
+        const std::set<Ship*, ShipNameCompare> & ships = nodeIt->first.second;
+        
+        for (std::set<Ship*, ShipNameCompare>::const_iterator shipIt = ships.begin();
+             shipIt != ships.end();
              ++shipIt) {
             if ((*shipIt)->canLoadCargo(cargo)) {
                 bestShip = *shipIt;
                 bestExtraCapacity = extraCapacity;
-                break;  
+                break;
             }
         }
         
-        if (bestShip != NULL && bestExtraCapacity == 0) {
-            break;  
+        if (bestExtraCapacity == 0) {
+            break;
         }
     }
     
