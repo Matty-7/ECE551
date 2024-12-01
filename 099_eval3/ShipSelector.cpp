c0185c6a2a0fed5ad5f957ef24a4435455b5e290
Matty-7 <jh730@duke.edu>
2024-11-30 21:31:47 -0500

    push

--- 099_eval3/ShipSelector.cpp
+++ 099_eval3/ShipSelector.cpp
@@ -9,42 +9,31 @@ Ship * ShipSelector::findBestShip(const Cargo & cargo) {
     Ship * bestShip = NULL;
     uint64_t minRemainingCapacity = std::numeric_limits<uint64_t>::max();
     
-    
     std::vector<std::pair<std::pair<uint64_t, std::set<Ship*, ShipNameCompare> >, int> > nodes = 
         shipMap.preOrderDump();
     
-    
-    size_t left = 0;
-    size_t right = nodes.size();
-    
-    while (left < right) {
-        size_t mid = left + (right - left) / 2;
-        uint64_t capacity = nodes[mid].first.first;
-        
-        if (capacity >= cargo.weight) {
-            right = mid;
-        } else {
-            left = mid + 1;
-        }
-    }
-    
-    
-    for (size_t i = left; i < nodes.size(); i++) {
+    for (size_t i = 0; i < nodes.size(); i++) {
         uint64_t remainingCapacity = nodes[i].first.first;
-        const std::set<Ship*, ShipNameCompare>& ships = nodes[i].first.second;
         
+        if (remainingCapacity < cargo.weight) {
+            continue;
+        }
         
         if (remainingCapacity - cargo.weight > minRemainingCapacity) {
-            break;
+            continue;
         }
         
+        const std::set<Ship*, ShipNameCompare>& ships = nodes[i].first.second;
         
         for (std::set<Ship*, ShipNameCompare>::const_iterator shipIt = ships.begin(); 
              shipIt != ships.end(); ++shipIt) {
             Ship * ship = *shipIt;
+            
             if (ship->canLoadCargo(cargo)) {
                 uint64_t remainingAfterLoad = remainingCapacity - cargo.weight;
-                if (remainingAfterLoad < minRemainingCapacity || 
+                
+                if (bestShip == NULL || 
+                    remainingAfterLoad < minRemainingCapacity || 
                     (remainingAfterLoad == minRemainingCapacity && ship->name < bestShip->name)) {
                     bestShip = ship;
                     minRemainingCapacity = remainingAfterLoad;
