8c3ab602e03a4dd24d07badbf43cf9af236068f3
Matty-7 <jh730@duke.edu>
2024-11-30 22:43:39 -0500

    did step4

--- 099_eval3/ShipSelector.cpp
+++ 099_eval3/ShipSelector.cpp
@@ -8,36 +8,31 @@ ShipSelector::ShipSelector(AVLMultiMap<uint64_t, Ship*, std::less<uint64_t>, Shi
 
 Ship * ShipSelector::findBestShip(const Cargo & cargo) {
     Ship * bestShip = NULL;
-    uint64_t minRemainingCapacity = std::numeric_limits<uint64_t>::max();
+    uint64_t bestRemainingCapacity = std::numeric_limits<uint64_t>::max();
     
     std::vector<std::pair<std::pair<uint64_t, std::set<Ship*, ShipNameCompare> >, int> > nodes = 
         shipMap.preOrderDump();
-        
-    size_t left = 0;
-    size_t right = nodes.size();
-    while (left < right) {
-        size_t mid = left + (right - left) / 2;
-        if (nodes[mid].first.first < cargo.weight) {
-            left = mid + 1;
-        } else {
-            right = mid;
-        }
-    }
     
-    for (size_t i = left; i < nodes.size(); ++i) {
+    for (size_t i = 0; i < nodes.size(); ++i) {
         uint64_t remainingCapacity = nodes[i].first.first;
+        
+        if (remainingCapacity < cargo.weight) {
+            continue;
+        }
+        
         uint64_t remainingAfterLoad = remainingCapacity - cargo.weight;
         
         const std::set<Ship*, ShipNameCompare>& ships = nodes[i].first.second;
         for (std::set<Ship*, ShipNameCompare>::const_iterator shipIt = ships.begin(); 
              shipIt != ships.end(); ++shipIt) {
             Ship * ship = *shipIt;
+            
             if (ship->canLoadCargo(cargo)) {
                 if (bestShip == NULL || 
-                    remainingAfterLoad < minRemainingCapacity || 
-                    (remainingAfterLoad == minRemainingCapacity && ship->name < bestShip->name)) {
+                    remainingAfterLoad < bestRemainingCapacity || 
+                    (remainingAfterLoad == bestRemainingCapacity && ship->name < bestShip->name)) {
                     bestShip = ship;
-                    minRemainingCapacity = remainingAfterLoad;
+                    bestRemainingCapacity = remainingAfterLoad;
                 }
             }
         }
