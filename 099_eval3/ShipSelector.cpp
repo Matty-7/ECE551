f5ab04706dde9f5caf64f33c13a4587e7708d8bd
Matty-7 <jh730@duke.edu>
2024-11-30 21:11:52 -0500

    Update ShipSelector

--- 099_eval3/ShipSelector.cpp
+++ 099_eval3/ShipSelector.cpp
@@ -8,31 +8,51 @@ ShipSelector::ShipSelector(AVLMultiMap<uint64_t, Ship*, std::less<uint64_t>, Shi
 Ship * ShipSelector::findBestShip(const Cargo & cargo) {
     Ship * bestShip = NULL;
     uint64_t minRemainingCapacity = std::numeric_limits<uint64_t>::max();
-
-    std::vector<std::pair<std::pair<uint64_t, std::set<Ship*, ShipNameCompare> >, int> > preOrderList = shipMap.preOrderDump();
-
-    for (size_t i = 0; i < preOrderList.size(); ++i) {
-        uint64_t shipRemainingCapacity = preOrderList[i].first.first;
-        std::set<Ship*, ShipNameCompare> ships = preOrderList[i].first.second;
-
-        for (std::set<Ship*, ShipNameCompare>::iterator shipIt = ships.begin(); shipIt != ships.end(); ++shipIt) {
+    
+    
+    std::vector<std::pair<std::pair<uint64_t, std::set<Ship*, ShipNameCompare> >, int> > nodes = 
+        shipMap.preOrderDump();
+    
+    
+    size_t left = 0;
+    size_t right = nodes.size();
+    
+    while (left < right) {
+        size_t mid = left + (right - left) / 2;
+        uint64_t capacity = nodes[mid].first.first;
+        
+        if (capacity >= cargo.weight) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+    
+    
+    for (size_t i = left; i < nodes.size(); i++) {
+        uint64_t remainingCapacity = nodes[i].first.first;
+        const std::set<Ship*, ShipNameCompare>& ships = nodes[i].first.second;
+        
+        
+        if (remainingCapacity - cargo.weight > minRemainingCapacity) {
+            break;
+        }
+        
+        
+        for (std::set<Ship*, ShipNameCompare>::const_iterator shipIt = ships.begin(); 
+             shipIt != ships.end(); ++shipIt) {
             Ship * ship = *shipIt;
             if (ship->canLoadCargo(cargo)) {
-                if (shipRemainingCapacity >= cargo.weight) {
-                    uint64_t remainingCapacityAfterLoading = shipRemainingCapacity - cargo.weight;
-                    if (remainingCapacityAfterLoading < minRemainingCapacity) {
-                        bestShip = ship;
-                        minRemainingCapacity = remainingCapacityAfterLoading;
-                    } else if (remainingCapacityAfterLoading == minRemainingCapacity) {
-                        if (ship->name < bestShip->name) {
-                            bestShip = ship;
-                        }
-                    }
+                uint64_t remainingAfterLoad = remainingCapacity - cargo.weight;
+                if (remainingAfterLoad < minRemainingCapacity || 
+                    (remainingAfterLoad == minRemainingCapacity && ship->name < bestShip->name)) {
+                    bestShip = ship;
+                    minRemainingCapacity = remainingAfterLoad;
                 }
             }
         }
     }
-
+    
     return bestShip;
 }
 
